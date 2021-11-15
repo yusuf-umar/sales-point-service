952e3245a6bdd5cd056195e2a526729c8693307d
tobey10 <fafowora.oluwatobiloba@academicianhelp.com>
2021-11-15 13:29:45 +0100

    Update routes.js

--- startup/routes.js
+++ startup/routes.js
@@ -3,13 +3,17 @@ const auth = require('../routes/auth');
 const user = require('../routes/user');
 const category = require('../routes/category');
 const ingredient = require('../routes/ingredient');
+const shop = require('../routes/shop');
+const menu = require('../routes/menu')
 
-module.exports = function(app){
+module.exports = function (app) {
     app.use(express.json());
     app.use("/auth", auth);
     app.use("/user", user);
-    app.use('/category',category);
+    app.use('/category', category);
     app.use('/ingredient', ingredient);
+    app.use('/shop', shop);
+    app.use('/menu', menu);
 
     app.use((req, res, next) => {
         return JsonResponse(res, 404, "API endpoint not found")
