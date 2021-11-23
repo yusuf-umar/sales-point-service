30931551c330deb959ea4ff265592c5cc12d37a8
tobey10 <fafowora.oluwatobiloba@academicianhelp.com>
2021-11-23 14:30:20 +0100

    category router fix

--- routes/category.js
+++ routes/category.js
@@ -2,16 +2,16 @@ const router = require("express").Router();
 const controller = require("../controllers");
 const { Auth, hasRole, ROLES } = require('../middlewares/auth');
 
-router.get("/", Auth,controller.category.getCategories);
+router.get("/", controller.category.getCategories);
 
-router.get("/admin",[Auth, hasRole(ROLES.ADMIN)],controller.category.getCategoriesByAdmin);
+router.get("/admin", [Auth, hasRole(ROLES.ADMIN)], controller.category.getCategoriesByAdmin);
 
-router.get("/:categoryId",Auth,controller.category.getCategory);
+router.get("/:categoryId", Auth, controller.category.getCategory);
 
-router.post("/",[Auth, hasRole(ROLES.ADMIN)],controller.category.createCategory);
+router.post("/", [Auth, hasRole(ROLES.ADMIN)], controller.category.createCategory);
 
-router.patch("/:categoryId",[Auth, hasRole(ROLES.ADMIN)],controller.category.updateCategory);
+router.patch("/:categoryId", [Auth, hasRole(ROLES.ADMIN)], controller.category.updateCategory);
 
-router.delete("/:categoryId",[Auth, hasRole(ROLES.ADMIN)],controller.category.deleteCategory);
+router.delete("/:categoryId", [Auth, hasRole(ROLES.ADMIN)], controller.category.deleteCategory);
 
 module.exports = router
