dd9a26e361529fa98252a74a808d345c7a35c36c
tobey10 <fafowora.oluwatobiloba@academicianhelp.com>
2021-12-07 12:49:28 +0100

    populate fix

--- services/cart.js
+++ services/cart.js
@@ -15,14 +15,14 @@ class CartService {
                     await cart.updateOne(
                         {
                             $inc: {
-                                quantity:1
+                                quantity: 1
                             }
                         }
                     )
 
                     return resolve(cart)
                 }
-                
+
                 const newOrder = new Cart(body)
                 await newOrder.save()
 
@@ -55,22 +55,38 @@ class CartService {
         return new Promise(async (resolve, reject) => {
             try {
                 const carts = await Cart.find(filter)
-                .populate({path: 'menu',populate:{path:'shop category'},populate:{path: 'ingredients',populate:{path:'ingredient'}}})
+                    .populate(
+                        {
+                            path: 'menu',
+                            populate: {
+                                path: 'shop category',
+                            }
+                        }
+                    )
+                    .populate(
+                        {
+                            path:'menu',
+                            populate: {
+                                path: 'ingredients',
+                                populate: { path: 'ingredient' }
+                            }
+                        }
+                    )
 
                 let total = 0;
                 let calories = 0;
                 let caloriesUnit = "";
 
-                if(carts.length > 1) {
-                    for(let i = 0; i < carts.length; i++) {
+                if (carts.length > 1) {
+                    for (let i = 0; i < carts.length; i++) {
                         let cartTotal = carts[i].amount * carts[i].quantity;
                         total += cartTotal;
                         caloriesUnit = carts[0].menu.ingredients[0].calorieUnit
 
-                        calories += this.calculateCalories(carts[i])    
+                        calories += this.calculateCalories(carts[i])
                     }
 
-                }else if (carts.length === 1){
+                } else if (carts.length === 1) {
                     total = carts[0].amount * carts[0].quantity
                     caloriesUnit = carts[0].menu.ingredients[0].calorieUnit
 
@@ -87,14 +103,14 @@ class CartService {
         })
     }
 
-    static calculateCalories(cart){
+    static calculateCalories(cart) {
         let calories = 0;
-        if(cart.menu.ingredients.length > 0){
-            for(let i=0;i<cart.menu.ingredients.length;i++){
+        if (cart.menu.ingredients.length > 0) {
+            for (let i = 0; i < cart.menu.ingredients.length; i++) {
                 let caloriesTotal = cart.menu.ingredients[i].calorie * cart.quantity;
                 calories += caloriesTotal
             }
-        }else if(cart.menu.ingredients.length === 1){
+        } else if (cart.menu.ingredients.length === 1) {
             calories = cart.menu.ingredients[0].calories * cart.quantity;
         }
 
@@ -104,17 +120,17 @@ class CartService {
     static removeCart(cartId, user) {
         return new Promise(async (resolve, reject) => {
             try {
-                const cart = await Cart.findOne({ 
+                const cart = await Cart.findOne({
                     _id: cartId,
                     user: user._id
                 })
-                if(!cart){
+                if (!cart) {
                     return reject({ statusCode: 404, msg: MSG_TYPES.NOT_FOUND })
                 }
 
                 await cart.delete()
 
-                resolve({msg: MSG_TYPES.DELETED})
+                resolve({ msg: MSG_TYPES.DELETED })
             } catch (error) {
                 console.log({ error })
                 reject({ statusCode: 500, msg: MSG_TYPES.SERVER_ERROR, error })
